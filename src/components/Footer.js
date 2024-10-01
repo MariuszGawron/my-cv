6800439e8ebe5ea0c58160ee1597e64b5c9f4dec
MariuszGawron <mariusz.gawron1@gmail.com>
2024-10-01 10:29:42 +0200

    Zmiana stopki

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -13,8 +13,10 @@ const Footer = () => {
   return (
     <footer className="footer">
       <span>
-        Wyrażam zgodę na przetwarzanie moich danych osobowych w celach rekrutacji, zgodnie z Ustawą z dn. 29 sierpnia 1997 r. o ochronie danych osobowych (Dz.
-        U. z 2002 r. Nr 101, poz. 926 z późniejszymi zmianami).
+        Wyrażam zgodę na przetwarzanie moich danych osobowych dla potrzeb niezbędnych do realizacji procesu rekrutacji (zgodnie z ustawą z dnia 10 maja 2018
+        roku o ochronie danych osobowych (Dz. Ustaw z 2018, poz. 1000) oraz zgodnie z Rozporządzeniem Parlamentu Europejskiego i Rady (UE) 2016/679 z dnia 27
+        kwietnia 2016 r. w sprawie ochrony osób fizycznych w związku z przetwarzaniem danych osobowych i w sprawie swobodnego przepływu takich danych oraz
+        uchylenia dyrektywy 95/46/WE (RODO)).
       </span>
       <h3>2024 - Mariusz Gawron</h3>
     </footer>
